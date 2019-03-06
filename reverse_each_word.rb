3946c19403b6fbe6c6f758effcccbe142757f46e
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-06 16:13:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  sentence_array.collect! do |word|
+  reversed_array = sentence_array.collect do |word|
      word.reverse
   end
-  sentence_array.join(" ")
+  reversed_array.join(" ")
 end
